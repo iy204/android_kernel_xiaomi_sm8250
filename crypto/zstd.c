77ec1b229582cfd95a18e1aee13f7ec4cd6b925a
Pzqqt <821026875@qq.com>
2025-03-15 00:09:58 +0900

    crypto: zstd: validate compression_level when it is set

--- crypto/zstd.c
+++ crypto/zstd.c
@@ -16,7 +16,27 @@
 
 
 static uint __read_mostly compression_level = 1;
-module_param(compression_level, uint, 0644);
+int set_compression_level(const char *val, const struct kernel_param *kp)
+{
+ 	int temp, ret;
+ 
+ 	ret = sscanf(val, "%i", &temp);
+ 	if (ret == -EINVAL) {
+ 		return -EINVAL;
+ 	}
+ 
+ 	if (temp == 0) {
+ 		temp = 1;
+ 	} else if (temp > zstd_max_clevel()) {
+ 		temp = zstd_max_clevel();
+ 	}
+ 
+ 	*((int *)kp->arg) = temp;
+ 
+ 	return 0;
+}
+ 
+module_param_call(compression_level, set_compression_level, param_get_int, &compression_level, 0644);
 
 struct zstd_ctx {
 	zstd_cctx *cctx;
