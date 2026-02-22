Rohail33/Realking_kernel_sm8250

serial: msm_geni_serial: Conditional IRQ enable/disable for specific xiaomi boards
The xiaomi boards ELISH, ENUMA, DAGU, and PIPA uses a seperate driver called
`msm_geni_serial_elish` in the stock, and following seems to be the only
difference between these two drivers.

Change-Id: I61b85fc3dc0a6a49a8d1d0759326a9ad0105cd36
Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/tty/serial/msm_geni_serial.c
@@ -2327,8 +2327,11 @@ static void msm_geni_serial_shutdown(struct uart_port *uport)
 		msm_geni_serial_stop_tx(uport);
 	}
 
+#if !defined(CONFIG_BOARD_ELISH) && !defined(CONFIG_BOARD_ENUMA) && \
+	!defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_BOARD_PIPA)
 	if (likely(!uart_console(uport)))
 		disable_irq(uport->irq);
+#endif
 
 	if (!uart_console(uport)) {
 		if (msm_port->ioctl_count) {
@@ -2469,8 +2472,12 @@ static int msm_geni_serial_startup(struct uart_port *uport)
 		}
 	}
 
+#if !defined(CONFIG_BOARD_ELISH) && !defined(CONFIG_BOARD_ENUMA) && \
+	!defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_BOARD_PIPA)
 	if (likely(!uart_console(uport)))
 		enable_irq(uport->irq);
+#endif
+
 	/*
 	 * Ensure that all the port configuration writes complete
 	 * before returning to the framework.
@@ -3537,6 +3544,9 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 	irq_set_status_flags(uport->irq, IRQ_NOAUTOEN);
 	ret = devm_request_irq(uport->dev, uport->irq, msm_geni_serial_isr,
 				IRQF_TRIGGER_HIGH, dev_port->name, uport);
+
+#if !defined(CONFIG_BOARD_ELISH) && !defined(CONFIG_BOARD_ENUMA) && \
+	!defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_BOARD_PIPA)
 	if (likely(!uart_console(uport))) {
 		/*
 		 * irq should disable untill msm_geni_serial_port_setup
@@ -3544,6 +3554,8 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 		 */
 		disable_irq(uport->irq);
 	}
+#endif
+
 	if (ret) {
 		dev_err(uport->dev, "%s: Failed to get IRQ ret %d\n",
 							__func__, ret);

iy204/android_kernel_xiaomi_sm8250

drivers/tty/serial/msm_geni_serial.c
@@ -2445,8 +2445,11 @@ static void msm_geni_serial_shutdown(struct uart_port *uport)
 		msm_geni_serial_stop_tx(uport);
 	}
 
+#if !defined(CONFIG_MACH_XIAOMI_ELISH) && !defined(CONFIG_MACH_XIAOMI_ENUMA) && \
+	!defined(CONFIG_MACH_XIAOMI_DAGU) && !defined(CONFIG_MACH_XIAOMI_PIPA)
 	if (likely(!uart_console(uport)))
 		disable_irq(uport->irq);
+#endif
 
 	if (!uart_console(uport)) {
 		if (msm_port->ioctl_count) {
@@ -2587,8 +2590,12 @@ static int msm_geni_serial_startup(struct uart_port *uport)
 		}
 	}
 
+#if !defined(CONFIG_MACH_XIAOMI_ELISH) && !defined(CONFIG_MACH_XIAOMI_ENUMA) && \
+	!defined(CONFIG_MACH_XIAOMI_DAGU) && !defined(CONFIG_MACH_XIAOMI_PIPA)
 	if (likely(!uart_console(uport)))
 		enable_irq(uport->irq);
+#endif
+
 	/*
 	 * Ensure that all the port configuration writes complete
 	 * before returning to the framework.
@@ -3659,6 +3666,9 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 	irq_set_status_flags(uport->irq, IRQ_NOAUTOEN);
 	ret = devm_request_irq(uport->dev, uport->irq, msm_geni_serial_isr,
 				IRQF_TRIGGER_HIGH, dev_port->name, uport);
+
+#if !defined(CONFIG_MACH_XIAOMI_ELISH) && !defined(CONFIG_MACH_XIAOMI_ENUMA) && \
+	!defined(CONFIG_MACH_XIAOMI_DAGU) && !defined(CONFIG_MACH_XIAOMI_PIPA)
 	if (likely(!uart_console(uport))) {
 		/*
 		 * irq should disable untill msm_geni_serial_port_setup
@@ -3666,6 +3676,8 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 		 */
 		disable_irq(uport->irq);
 	}
+#endif
+
 	if (ret) {
 		dev_err(uport->dev, "%s: Failed to get IRQ ret %d\n",
 							__func__, ret);