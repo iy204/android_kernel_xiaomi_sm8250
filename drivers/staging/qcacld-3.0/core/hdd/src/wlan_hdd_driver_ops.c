8a6255e824472a5da31c0c42f2fd10e1376ed863
ApartTUSITU <aptusitu@outlook.com>
2026-03-14 20:03:32 +0800

    Revert "BACKPORT: qcacld-3.0: unblock the state control param thread at end of SSR"
    This reverts commit a74365438e6df7ab84bb35bbde2e15507d6edb61.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_driver_ops.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_driver_ops.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2015-2020 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -663,6 +662,7 @@ static int __hdd_soc_recovery_reinit(struct device *dev,
 	}
 
 	hdd_soc_load_unlock(dev);
+	hdd_start_complete(0);
 
 	return 0;
 
@@ -713,7 +713,6 @@ static int hdd_soc_recovery_reinit(struct device *dev,
 
 
 	osif_psoc_sync_trans_stop(psoc_sync);
-	hdd_start_complete(0);
 
 	return errno;
 }
